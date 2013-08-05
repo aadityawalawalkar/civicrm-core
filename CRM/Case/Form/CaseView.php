df9aa043de105f1e4afad2a450ce5b3ffd3eacf3
Aaditya Walawalkar <aaditya.walawalkar@webaccess.co.in>
2013-08-05 15:29:10 +0530

    Changes the fix for CRM-12812 & corrected a syntax error in ActivityChangeStatus.js file
    ----------------------------------------
    * CRM-12812: case custom field using wysiwyg fails to load
      http://issues.civicrm.org/jira/browse/CRM-12812
    * CRM-12439-org:
      http://issues.civicrm.org/jira/browse/CRM-12439-org
    * CRM-12439: Add description field for Profiles and expose the created_id field
      http://issues.civicrm.org/jira/browse/CRM-12439
    * CRM-12995-1:
      http://issues.civicrm.org/jira/browse/CRM-12995-1
    * CRM-12995-2:
      http://issues.civicrm.org/jira/browse/CRM-12995-2

--- CRM/Case/Form/CaseView.php
+++ CRM/Case/Form/CaseView.php
@@ -53,8 +53,6 @@ class CRM_Case_Form_CaseView extends CRM_Core_Form {
   public function preProcess() {
     // js for changing activity status
     CRM_Core_Resources::singleton()->addScriptFile('civicrm', 'templates/CRM/Case/Form/ActivityChangeStatus.js');
-    // js for showing rich text editor
-    CRM_Core_Resources::singleton()->addScriptFile('civicrm', 'packages/ckeditor/ckeditor.js');
 
     $this->_showRelatedCases = CRM_Utils_Array::value('relatedCases', $_GET);
 
